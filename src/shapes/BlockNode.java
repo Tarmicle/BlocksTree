b10bf2de4dea7f53780b27bfe7f68372bdde219d
Tarmicle <enrico.bottani@hotmail.com>
2018-10-03 20:23:26 +0200

    Parser fix

---
+++ src/shapes/BlockNode.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public abstract class BlockNode {
+    public abstract int width();
+    public abstract  int height();
+
+    public int area(){
+        return width()*height();
+    }
+    public abstract void draw();
+
+    protected abstract void genBitmap(int[][] bitmap,int x,int y);
+    public BlockDrawer initBitMap(){
+        return new BlockDrawer(this);
+    }
+
+}
